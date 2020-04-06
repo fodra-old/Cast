6180b72711af74d0e199a11a670b5bb0cbeb4762
Joao Marcos Fodra Veloso <jmveloso@outlook.com.br>
2020-04-06 20:09:44 -0300

    Serv.Contr. para categoria

--- BackEnd/src/main/java/br/com/cast/avaliacao/controller/CategoriaController.java
+++ BackEnd/src/main/java/br/com/cast/avaliacao/controller/CategoriaController.java
@@ -1,8 +1,41 @@
 package br.com.cast.avaliacao.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.cast.avaliacao.model.Categoria;
+import br.com.cast.avaliacao.service.CategoriaService;
+
 @RestController
+@RequestMapping("/categoria")
 public class CategoriaController {
 
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	/**
+	 * @see CategoriaService#findById(Integer)
+	 * @param id
+	 * @return
+	 */
+	@GetMapping()
+	public Categoria findById(@RequestParam("id") Integer id) {
+		
+		return categoriaService.findById(id);
+	}
+	
+	/**
+	 * @see CategoriaService#findAll()
+	 * @return
+	 */
+	@GetMapping("/all")
+	public List<Categoria> findAll() {
+		
+		return categoriaService.findAll();
+	}
 }
--- BackEnd/src/main/java/br/com/cast/avaliacao/service/CategoriaService.java
+++ BackEnd/src/main/java/br/com/cast/avaliacao/service/CategoriaService.java
@@ -1,8 +1,34 @@
 package br.com.cast.avaliacao.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.cast.avaliacao.model.Categoria;
+import br.com.cast.avaliacao.repository.CategoriaRepository;
 
 @Service
 public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	/**
+	 * Retorna a categoria de acordo com o ID
+	 * @param id
+	 * @return
+	 */
+	public Categoria findById(Integer id) {
+		return categoriaRepository.getOne(id);
+	}
+	/**
+	 * Retorna todas as categorias cadastradas
+	 * @return
+	 */
+	public List<Categoria> findAll() {
+
+		return categoriaRepository.findAll();
+	}
 
 }
