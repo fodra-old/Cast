cfc63680e9a47afffdd14bdfa6fdf37b6e4f21ec
Joao Marcos Fodra Veloso <jmveloso@outlook.com.br>
2020-04-06 21:59:26 -0300

    Comentarios Serviço Curso

--- BackEnd/src/main/java/br/com/cast/avaliacao/service/CursoService.java
+++ BackEnd/src/main/java/br/com/cast/avaliacao/service/CursoService.java
@@ -18,11 +18,21 @@ public class CursoService {
 	@Autowired
 	private CursoRepository cursoRepository;
 	
+	/**
+	 * Retorna lista de cursos da categoria informada no parametro
+	 * @param categoria
+	 * @return
+	 */
 	public List<Curso> findByCategoria(Categoria categoria) {
 	
 		return Lists.newArrayList(cursoRepository.findAll(QCurso.curso.categoria.eq(categoria)));
 	}
 	
+	/**
+	 * Salva registro de curso informado no parametro
+	 * @param curso
+	 * @return
+	 */
 	public Curso salvar(Curso curso) {
 		
 		cursoRepository.save(curso);
