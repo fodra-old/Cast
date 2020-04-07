a646d1f5282b8fe6632b02fd75229355e033d71c
Joao Marcos Fodra Veloso <jmveloso@outlook.com.br>
2020-04-06 21:57:58 -0300

    Endpoint para Cursos

--- BackEnd/src/main/java/br/com/cast/avaliacao/controller/CursoController.java
+++ BackEnd/src/main/java/br/com/cast/avaliacao/controller/CursoController.java
@@ -1,8 +1,44 @@
 package br.com.cast.avaliacao.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.cast.avaliacao.model.Categoria;
+import br.com.cast.avaliacao.model.Curso;
+import br.com.cast.avaliacao.service.CursoService;
+
 @RestController
+@RequestMapping("/curso")
 public class CursoController {
 
+	@Autowired
+	private CursoService cursoService;
+	
+	/**
+	 * @see CursoService#findByCategoria(Categoria)
+	 * @param categoria
+	 * @return
+	 */
+	@GetMapping
+	public List<Curso> findByCategoria(@ModelAttribute("categoria") Categoria categoria) {
+		
+		return cursoService.findByCategoria(categoria);
+	}
+	
+	/**
+	 * @see CursoService#salvar(Curso)
+	 * @param curso
+	 * @return
+	 */
+	@PostMapping
+	public Curso salvar(@ModelAttribute("curso") Curso curso) {
+		
+		return cursoService.salvar(curso);
+	}
 }
--- BackEnd/src/main/java/br/com/cast/avaliacao/service/CursoService.java
+++ BackEnd/src/main/java/br/com/cast/avaliacao/service/CursoService.java
@@ -1,8 +1,31 @@
 package br.com.cast.avaliacao.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.google.common.collect.Lists;
+
+import br.com.cast.avaliacao.model.Categoria;
+import br.com.cast.avaliacao.model.Curso;
+import br.com.cast.avaliacao.model.QCurso;
+import br.com.cast.avaliacao.repository.CursoRepository;
 
 @Service
 public class CursoService {
 
+	@Autowired
+	private CursoRepository cursoRepository;
+	
+	public List<Curso> findByCategoria(Categoria categoria) {
+	
+		return Lists.newArrayList(cursoRepository.findAll(QCurso.curso.categoria.eq(categoria)));
+	}
+	
+	public Curso salvar(Curso curso) {
+		
+		cursoRepository.save(curso);
+		return curso;
+	}
 }
