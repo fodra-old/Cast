862cb9885de46d761233bcc0c166f356a57503bf
Joao Marcos Fodra Veloso <jmveloso@outlook.com.br>
2020-04-11 08:36:45 -0300

    Atualização Properties

--- BackEnd/src/main/java/br/com/cast/avaliacao/CastApplicationStarter.java
+++ BackEnd/src/main/java/br/com/cast/avaliacao/CastApplicationStarter.java
@@ -35,6 +35,7 @@ public class CastApplicationStarter extends SpringBootServletInitializer impleme
 	 */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+        	.allowedMethods("*");
     }
 }
